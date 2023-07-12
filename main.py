9f7482291cf817b39026818562ecc8be4a85e51d
guilherme machado <guilherme.machado@indicium.tech>
2023-07-12 09:15:52 -0300

    adding new code to main

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import os
 
 def main():
     json_data = WebScraper(url='https://steamdb.info/sales/').extract_soup_data()
-    bigquery = BigQuery('steam-summer-sale', 'summer_sales_data', 'tn_sales_2023')
 
-    return bigquery.load_json_data_to_table(json_data=json_data)
+    bq = BigQuery('summer-sales-extraction1', 'summer_sales_data', 'tn_sales_2023')
+    bq.create_dataset()
+    bq.create_table()
+    bq.load_json_data_to_table(json_data=json_data)
 
 
 if __name__ == "__main__":
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '../web_to_bigquery/models/config.json'
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'config.json'
     main()
