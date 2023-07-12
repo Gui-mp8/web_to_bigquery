eae7a416efcaafd527c555ec7bbaf297e9396452
guilherme machado <guilherme.machado@indicium.tech>
2023-07-12 09:13:45 -0300

    correcting 2 methods, create_dataset and create_table

--- models/bigquery.py
+++ models/bigquery.py
@@ -2,7 +2,6 @@ from google.cloud import bigquery
 import json
 from typing import Dict, Any
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'config.json'
 
 class BigQuery():
     def __init__(self, project_name:str, dataset_name:str, table_name:str) -> None:
@@ -11,13 +10,16 @@ class BigQuery():
         self.table_id = f"{project_name}.{dataset_name}.{table_name}"
 
     def create_dataset(self) -> bigquery.Dataset:
-        dataset = self.client.get_dataset(self.dataset_id)
+        try:
+            dataset = self.client.get_dataset(self.dataset_id)
+        except Exception:
+            dataset = False
 
         if not dataset:
             dataset = bigquery.Dataset(self.dataset_id)
             dataset.location = "US"
+            dataset = self.client.create_dataset(dataset)
 
-            dataset = self.client.create_dataset(dataset)  # Make an API request.
             print(f"Created dataset {self.dataset_id}")
         else:
             print(f"Dataset {self.dataset_id} already exists")
@@ -29,10 +31,14 @@ class BigQuery():
             bigquery.SchemaField("price", "STRING", mode="REQUIRED"),
             bigquery.SchemaField("rating", "STRING", mode="REQUIRED"),
         ]
-
         table_path = bigquery.Table(self.table_id, schema=schema)
+        
+        try:
+            table = self.client.get_table(table_path, retry=None)
+        except Exception:
+            table = False
 
-        if not self.client.get_table(table_path, retry=None):
+        if not table:
             table = self.client.create_table(table_path)  # Make an API request.
             print(f"Created table {table.full_table_id}")
         else:
@@ -55,6 +61,3 @@ class BigQuery():
                     print(error)
         else:
             print("Table is not empty. Skipping data load.")
-
-bq = BigQuery('steam-summer-sale', 'summer_sales_data', 'tn_sales_2023')
-bq.create_table()
