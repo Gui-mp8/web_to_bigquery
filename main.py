a28df86d55eb0af51d2fa091428a4980356fc487
Guilherme Machado <guilherme.machado@indicium.tech>
2023-07-12 22:23:23 -0300

    updating main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from models.bigquery import BigQuery
 from models.web_scraper import WebScraper
 import os
 
-def main():
+def main(project_name, dataset_name, table_name):
     json_data = WebScraper(url='https://steamdb.info/sales/').extract_soup_data()
 
-    bq = BigQuery('summer-sales-extraction1', 'summer_sales_data', 'tn_sales_2023')
+    bq = BigQuery(project_name, dataset_name, table_name)
     bq.create_dataset()
     bq.create_table()
     bq.load_json_data_to_table(json_data=json_data)
@@ -13,4 +13,9 @@ def main():
 
 if __name__ == "__main__":
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'config.json'
-    main()
+    main(
+        project_name='summer-sales-extraction1',
+        dataset_name='summer_sales_data',
+        table_name='tn_sales_2023'
+    )
+
