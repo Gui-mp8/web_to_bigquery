5471c0f6021bbbeb464e59da99b7bccfc9a0ca0f
guilherme machado <guilherme.machado@indicium.tech>
2023-07-13 11:43:40 -0300

    updatng to show which is the error

--- models/web_scraper.py
+++ models/web_scraper.py
@@ -27,11 +27,6 @@ class WebScraper():
     def __init__(self, url: str) -> None:
         self.url = url
         self.session = None
-        self.headers = {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "cookie": "cf_clearance=k4R7jAe2EKTktPjEgATVzuehYw.SHgoYKYO8aL7It6s-1689216169-0-160; __cf_bm=AC16fBICbYUwSK.G.ce9sRT0I0KQ62fD1GdU4_AcI00-1689217358-0-AZhkYuQPFWxQz/Om1JR4Z9KvKdgsZSaYP9NruJChDHglGOt/E4jI3aRzn/rqtbOJtOmcjKHb9HisZBZk9R1JvJs=",
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-        }
         self.logger = logging.getLogger(__name__)
 
     def create_session(self) -> Session:
@@ -49,7 +44,20 @@ class WebScraper():
         return session
 
     def response(self) -> requests.Response:
-        return self.create_session().get(url=self.url, headers=self.headers)
+        headers = {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "cookie": "cf_clearance=W5hoD.mM4zzqMAMgk1IutY3NkRv0flJp1ro6zd9cKQM-1689161684-0-160; __cf_bm=2LUm5F5Ziw0Zrfl7MS62b9xBkwjziROWKoWtQyiLlp8-1689252045-0-AXFz599NwACtjBOsXiP16LZjUSm/xsQYaxNzGf6VCYI6CnmhU0Lz2oUa/9nRdTJKXh4jruwbAGB0mIO6Hrxys8U=",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0"
+        }
+        try:
+            response = self.create_session().get(url=self.url, headers=headers)
+            if response.status_code != 200:
+                logger.error("The error is on headers, you must change the 'accept', 'cokie' and 'user-agent' params")
+            logger.info(f'STATUS CODE: {response.status_code}')
+        except Exception:
+            raise
+
+        return response
 
     def soup(self) -> BeautifulSoup:
         return BeautifulSoup(self.response().content, 'html.parser')
@@ -72,15 +80,4 @@ class WebScraper():
             data_list_dict.append(data_sort_dict)
             self.logger.info(data_sort_dict)
 
-        # return json.dumps(data_list)
         return data_list_dict
-
-# from datetime import datetime
-# json_data = WebScraper(url='https://steamdb.info/sales/').extract_soup_data()
-# for item in json_data:
-#     item['price'] = item['price'][:-2] + '.' + item['price'][-2:]
-#     item['end_date'] = datetime.fromtimestamp(int(item['end_date'])).strftime('%Y-%m-%d')
-#     item['start_date'] = datetime.fromtimestamp(int(item['start_date'])).strftime('%Y-%m-%d')
-#     item['release_date'] = datetime.fromtimestamp(int(item['release_date'])).strftime('%Y-%m-%d')
-
-# print(json_data)
